f9a28a01d7429f49bf5ab331f601520ba804c1f7
Kainat Abbasi <KainatAkmal2003@gmail.com>
2024-03-06 00:33:01 -0800

    DayOne

---
+++ Day_01/Question_01.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("Excited for 100 days of typescript challenge ");
+//installation of VS code , Node.js, Typescript
+console.log("I have set the environment for typescript !!");
---
+++ Day_01/Question_03.js
@@ -0,0 +1,6 @@
+"use strict";
+//Name cases: store a person name in a variable , and then print that person's name in lowercase, uppercase, and titlecase
+let name2 = "kainat"; // this line store person name
+console.log(name2.toLowerCase()); //this method shows name in small letters
+console.log(name2.toUpperCase()); // this method shows name in capital letter
+console.log(name2.charAt(0).toUpperCase() + name2.slice(1).toLowerCase()); // this method shows first name in capital letter
