0947d2778bfdc02eccd970e2961aa85dc3b7a693
Kainat Abbasi <KainatAkmal2003@gmail.com>
2024-03-06 00:33:01 -0800

    DayOne

---
+++ Day_01/Question_01.ts
@@ -0,0 +1,5 @@
+console.log("Excited for 100 days of typescript challenge ");
+
+//installation of VS code , Node.js, Typescript
+console.log("I have set the environment for typescript !!");
+
---
+++ Day_01/Question_02.ts
@@ -0,0 +1,4 @@
+//personal message: store a person name in a variable and print a message to them, like "Hello Kainat, would you like to learn some Typescript today ?"
+
+let name1 : string = "kainat";
+console.log(`Hello ${name1}, would you like to learn some Typescript today ?`);
