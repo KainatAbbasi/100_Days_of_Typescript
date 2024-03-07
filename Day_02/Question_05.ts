1406e1103c547efbbbf1766b0493e77931cdc2bc
Kainat Abbasi <KainatAkmal2003@gmail.com>
2024-03-06 23:25:02 -0800

    updated

---
+++ Day_02/Question_05.ts
@@ -0,0 +1,4 @@
+// famous quote : Repeat exercise 1,but this time store the famous person name in a variable called famous person .then print your quote in other variable
+let famous_person :string = 'albert Einstien';
+let famous_quote :string = 'once you stop learning, you start Dying !';
+console.log(`${famous_person} said, ${famous_quote}`)
---
+++ Day_03/Question_09.ts
@@ -0,0 +1,4 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favorite_number : number = 7;  //this variable store my fav number
+console.log(`My favorite number is : ${favorite_number}.`);  
