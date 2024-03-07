657814950af8a7a86c826abc7dab75511007d4db
Kainat Abbasi <KainatAkmal2003@gmail.com>
2024-03-06 23:25:02 -0800

    DayTwo

---
+++ Day_02/Question_05.js
@@ -0,0 +1,5 @@
+"use strict";
+// famous quote : Repeat exercise 1,but this time store the famous person name in a variable called famous person .then print your quote in other variable
+let famous_person = 'albert Einstien';
+let famous_quote = 'once you stop learning, you start Dying !';
+console.log(`${famous_person} said, ${famous_quote}`);
