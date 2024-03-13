844d500065d2b76795ec3a23bb1b67a73f55c045
Kainat Abbasi <KainatAkmal2003@gmail.com>
2024-03-13 02:15:51 -0700

    Added new Repository

---
+++ Day_10/Question_29.ts
@@ -0,0 +1,24 @@
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+let favFruit :string[] = ['Apple', 'Strawberries', "Banana"];
+
+if(favFruit.includes('Apple') ){
+    console.log("You really like Apple !");
+}
+
+if(favFruit.includes('Banana') ){
+    console.log("You really like Banana's !");
+}
+
+if(favFruit.includes('Orange') ){
+    console.log("You really like Orange !");
+}
+
+if(favFruit.includes('Strawberries') ){
+    console.log("You really like Strawberries !");
+}
+
+if(favFruit.includes('Peach') ){
+    console.log("You really like Peach !");
+}
---
+++ Day_11/Question_31.ts
@@ -0,0 +1,17 @@
+// No Users:make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+
+let userList: string[] = ["user1", "user2", "user3"];
+
+// Check if the list of users is not empty
+if (userList.length > 0) {
+  // Remove all usernames from the array
+  userList = [];
+
+  // Print the appropriate message
+  console.log("All usernames have been removed.");
+} else {
+  // Print a message if the list is empty
+  console.log("We need to find some users!");
+}
